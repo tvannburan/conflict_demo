786398f29b7fc796a8e9978ed9fd53c7d9eda50a
Thadius Vannburan <thadiusv@gmail.com>
2021-12-01 22:51:03 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 0bbd98fdb5e3acdc5a6e3d77a6f6fbfdeb54aa82
+    hello_world('NYC', 'New York')
 
 
 main()
